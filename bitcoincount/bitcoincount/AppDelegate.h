7d419459129a034763fd47c32618f1ccff8691e6
chenxuan 's dev <xuanchen@xuantekiiMac.local>
2015-07-04 14:53:34 +0800

    0.0.1

---
+++ bitcoincount/bitcoincount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bitcoincount
+//
+//  Created by xuan chen on 15/7/4.
+//  Copyright (c) 2015年 xuan chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
